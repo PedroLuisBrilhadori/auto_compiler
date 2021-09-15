bdab02d38a2bbd3b10f842a0eeb03feb9dfbdf0a
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-09-15 13:28:10 -0300

    melhorando configuração inicial

--- codigo-fonte/setup.cpp
+++ codigo-fonte/setup.cpp
@@ -2,16 +2,55 @@
 
 using namespace std;
 
+char config_comando_compilador[15];
+
 void busca_atualizacoes() {
     // int teste = system("git pull");
     // cout << teste << endl;
 }
 
+void criar_path(){
+
+}
+
+void instalar_path(){
+
+}
+
+void compilador_config() {
+    char flag, flag2;
+    bool config_compilador = false;
+
+    do{
+        cout << "Primeiro, vamos compilar nosso código fonte! \n";
+        cout << "Seu computador possui um compilador instalado? [Y/n] "; 
+        fflush(stdin); cin.get(flag);
+
+        if(toupper(flag) != 'N'){
+            cout << "Beleza! Conta pra gente, qual é o comando de seu compilador? \nExemplo: g++\n";
+            cin >> config_comando_compilador;
+            cout << "ok! O comando de seu compilador é: '" << config_comando_compilador << "' certo? [Y/n] ";
+            fflush(stdin); cin.get(flag2);
+            if(toupper(flag2) != 'N')
+                config_compilador = true;
+            else{
+                cout << "Preciso de um compilador! Vamos tentar novamente\n"; 
+                fflush(stdin); getchar();
+            }
+        } else {
+            cout << "Vamos tentar novamente...\n"; 
+            fflush(stdin); getchar();
+        }
+    } while(config_compilador == false);
+    
+    
+}
 
 int main() {
-    char verifica_att = 'y', flag;
-    bool config_compilador;
-    char config_comando_compilador[15];
+    system("chcp 65001");
+    system("cls");
+    char verifica_att = 'y', flag, flag2;
+
 
 
     cout << "Olá, eu sou o instalador do app bacana que compila as coisas automaticamente \n";
@@ -25,18 +64,7 @@ int main() {
         cout << "------------------------------------------\n\n"; 
     }
 
-    cout << "Primeiro, vamos compilar nosso código fonte! \n";
-    cout << "Seu computador possui um compilador instalado? [Y/n] "; 
-    fflush(stdin); cin.get(flag);
-    
-    if(toupper(flag) != 'N'){
-        cout << "Beleza! Conta pra gente, qual é o comando de seu compilador? \nExemplo: g++\n";
-        cin >> config_comando_compilador;
-    } else {
-        cout << "Você deve instalar um compilador, recomendo o MinGW\n";
-        exit(0);
-    }
-
+    compilador_config();
 
 
     return 0;
